142d2a796e31633aee35b480c5b4db79401e5bd4
briannovillo <brian@briannovillo.com.ar>
2019-02-19 12:18:50 -0300

    Add ffmpeg command

--- test/record-from-speaker.sh
+++ test/record-from-speaker.sh
@@ -7,5 +7,6 @@ DEFAULT_SPEAKER=$(pactl list short sources | grep output | awk -F" " '{ print $2
 while :
 do
   echo "Recording speakers"
-  avconv -f pulse -i ${DEFAULT_SPEAKER} -t 10 record.wav -y
+  #avconv -f pulse -i ${DEFAULT_SPEAKER} -t 10 record.wav -y
+  ffmpeg -f pulse -i ${DEFAULT_SPEAKER} -t 10  record.wav -y
 done
